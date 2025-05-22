ebeea7ffd695453b0e22527f0972e0cd0e020640
L V Ramana <lellalvramana@gmail.com>
2025-05-22 05:53:44 +0530

    updated roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -6,20 +6,23 @@ Instances=( "mongodb" "catalogue" "mysql" "redis" "cart" "user" "rabbitmq" "ship
 Zone-ID="Z03781442EPPF9VGY8NMT"
 Domai_name="ramana.site"
 
-for instance in ${Instances[@]}
+
+#for instance in ${INSTANCES[@]} passing from array
+
+for instance in $@ #for isntances passing like function arguments 
 do 
     
-  instance_id=$(aws ec2 run-instances \
+instance_id= $(aws ec2 run-instances \
 --image-id ami-09c813fb71547fc4f \
 --instance-type t3.micro \
 --security-group-ids sg-0eccd877a911ed1e3 \
---tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value= $instance}]' ) 
-
+--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"--query "Instances[0].InstanceId" --output text)
+   
 if [ $instance != "frontend" ]
 then 
     IP=$(aws ec2 describe-instances --instance-ids $instance_id --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
 else
-     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+     IP=$(aws ec2 describe-instances --instance-ids $instance_id --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
 fi
     echo "$instance IP address: $IP"
 done

2f9fec557eb286008f71acbff8367f2219ee4602
L V Ramana <lellalvramana@gmail.com>
2025-05-24 12:27:11 +0530

    creating instances

--- roboshop.sh
+++ roboshop.sh
@@ -2,7 +2,7 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 Sg_ID="sg-0eccd877a911ed1e3"
-Instances=( "mongodb" "catalogue" "mysql" "redis" "cart" "user" "rabbitmq" "shipping" "payment" "dispatch" "frontend")
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 Zone_ID="Z03781442EPPF9VGY8NMT"
 Domain_name="ramana.site"
 script_name= echo $0|cut -d '.' -f1
@@ -10,20 +10,16 @@ script_name= echo $0|cut -d '.' -f1
 
 #for instance in ${INSTANCES[@]} passing from array
 
-for instance in ${Instances[@]} #for isntances passing like function arguments 
+for instance in ${INSTANCES[@]} #for isntances passing like function arguments 
 do 
     
-instance_id= $(aws ec2 run-instances \
---image-id $AMI_ID \
---instance-type t3.micro \
---security-group-ids $Sg_ID \
---tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"--query "Instances[0].InstanceId" --output text)
+INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
    
 if [ $instance != "frontend" ]
 then 
-    IP=$(aws ec2 describe-instances --instance-ids $instance_id --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
 else
-     IP=$(aws ec2 describe-instances --instance-ids $instance_id --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
 fi
     echo "$instance IP address: $IP"
 done

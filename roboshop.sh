2681210295fb9b6042d88b708f5a5433de036ac7
L V Ramana <lellalvramana@gmail.com>
2025-05-24 12:17:40 +0530

    creating instances

--- roboshop.sh
+++ roboshop.sh
@@ -4,19 +4,20 @@ AMI_ID="ami-09c813fb71547fc4f"
 SG-ID="sg-0eccd877a911ed1e3"
 Instances=( "mongodb" "catalogue" "mysql" "redis" "cart" "user" "rabbitmq" "shipping" "payment" "dispatch" "frontend")
 Zone-ID="Z03781442EPPF9VGY8NMT"
-Domai_name="ramana.site"
+Domain_name="ramana.site"
+script_name= echo $0|cut -d '.' -f1
 
 
 #for instance in ${INSTANCES[@]} passing from array
 
-for instance in $@ #for isntances passing like function arguments 
+for instance in $($Instances[@]) #for isntances passing like function arguments 
 do 
     
-instance_id= $(aws ec2 run-instances \
---image-id ami-09c813fb71547fc4f \
---instance-type t3.micro \
---security-group-ids sg-0eccd877a911ed1e3 \
---tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"--query "Instances[0].InstanceId" --output text)
+# instance_id= $(aws ec2 run-instances \
+# --image-id ami-09c813fb71547fc4f \
+# --instance-type t3.micro \
+# --security-group-ids sg-0eccd877a911ed1e3 \
+# --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"--query "Instances[0].InstanceId" --output text)
    
 if [ $instance != "frontend" ]
 then 
@@ -27,10 +28,10 @@ fi
     echo "$instance IP address: $IP"
 done
 
-aws ec2 run-instances \
---image-id ami-09c813fb71547fc4f \
---instance-type t3.micro \
---key-name demo-key \
---security-group-ids sg-0eccd877a911ed1e3 \
---tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=demo-server}]' 
+# aws ec2 run-instances \
+# --image-id ami-09c813fb71547fc4f \
+# --instance-type t3.micro \
+# --key-name demo-key \
+# --security-group-ids sg-0eccd877a911ed1e3 \
+# --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=demo-server}]' 
 

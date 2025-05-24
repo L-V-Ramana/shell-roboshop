b07a107745ba0d6bf6e312256eaaf51da75e95fe
L V Ramana <lellalvramana@gmail.com>
2025-05-24 12:41:25 +0530

    creating instances

--- roboshop.sh
+++ roboshop.sh
@@ -20,10 +20,31 @@ INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro 
 if [ $instance != "frontend" ]
 then 
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    RECORD_NAME=$instance.$Domain_name
 else
      IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+     RECORD_NAME=$Domain_name
 fi
     echo "$instance IP address: $IP"
+    RECORD_NAME=$instance.$Domain_name
+
+     aws route53 change-resource-record-sets \
+    --hosted-zone-id $Zone_ID \
+    --change-batch '
+    {
+        "Comment": "Creating or Updating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }'
 done
 
 # aws ec2 run-instances \
